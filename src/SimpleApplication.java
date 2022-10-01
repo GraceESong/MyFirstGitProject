50c748a3fae25b91718e26befc30a5f794642b37
GraceESong <song.grace@hotmail.com>
2022-09-30 22:51:33 -0400

    this is my SECOND commit

--- src/SimpleApplication.java
+++ src/SimpleApplication.java
@@ -2,7 +2,7 @@
 public class SimpleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Let us write a guide to use a git for Java project");
+		System.out.println("This is change for Second commit");
 	}
 
 }
